7a8366d5aee2d97f5b57a4dabba1f54b0818e918
LuK1337 <priv.luk@gmail.com>
2026-02-12 17:34:21 +0000

    sdm660-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- common.mk
+++ common.mk
@@ -175,6 +175,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
 PRODUCT_PACKAGES += \
+    NcmTetheringOverlay \
     Sdm660CarrierConfigOverlay \
     Sdm660DialerOverlay \
     Sdm660FrameworksOverlay \
