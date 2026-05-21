fce3e6164b95a2da542e49e93d27780966eed531
Tuan Anh <tuan73176@gmail.com>
2026-05-21 23:52:16 +0000

    sdm660-common: Migrate Megvii shim to compat
    Change-Id: I81697f6bee1ea3ea07d2e9a02fc88a00ec4ea025

--- camera/megvii_shim.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: The LineageOS Project
- * SPDX-License-Identifier: Apache-2.0
- */
-
-namespace android {
-    extern "C" void mg_facepp() {}
-    extern "C" void mg_beautify() {}
-}
