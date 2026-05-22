f8b83851a95d329caacd13a1b0ba35a067cfc3cd
LuK1337 <priv.luk@gmail.com>
2026-05-22 00:15:19 +0000

    sdm660-common: Mass blob update from HON660-T-96.00.05-(0092)
    Change-Id: I30e6377f03738a957153b2e781f85644f3c3c79c

--- extract-files.py
+++ extract-files.py
@@ -9,7 +9,6 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -20,10 +19,11 @@ from extract_utils.main import (
 
 namespace_imports = [
     'device/nokia/sdm660-common',
-    'hardware/qcom-caf/msm8998',
+    'hardware/qcom-caf/sdm660',
     'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
-    'vendor/qcom/opensource/data-ipa-cfg-mgr-legacy-um',
+    'vendor/qcom/opensource/display',
 ]
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
@@ -41,8 +41,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    ('vendor/lib64/mediadrm/libwvdrmengine.so','vendor/lib64/libwvhidl.so'): blob_fixup()
-        .add_needed('libcrypto_shim.so'),
+    'vendor/etc/init/android.hardware.gnss@2.1-service-qti.rc': blob_fixup()
+        .regex_replace('    disabled', '    #disabled'),
     ('vendor/etc/msm_irqbalance.conf', 'vendor/etc/msm_irqbalance_sdm630.conf'): blob_fixup()
         .regex_replace('IGNORED_IRQ=19,22,39,200,203\n', 'IGNORED_IRQ=19,22,39,115,200,203,332\n'),
     'system_ext/lib64/lib-imscamera.so': blob_fixup()
